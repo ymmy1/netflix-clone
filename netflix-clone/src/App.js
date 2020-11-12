2c3560c203120b3b6bbe30c18a875e2546e840e1
ymmy1 <chem-pion@mail.ru>
2020-11-12 01:08:52 -0800

    row component

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>hey</h2>
+      <Row title="NETFLIX ORIGINALS" fetchUrl ={requests.fetchNetflixOriginals} />
+      <Row title="Tranding Now" fetchUrl ={requests.fetchTrending} />
     </div>
   );
 }
